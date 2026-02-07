0e84b941afb97b3f04dd61cddead7f3bda0c8c56
Shivam-RN <shivamrana1809@gmail.com>
2026-02-07 13:06:36 +0530

    Project-Setup

---
+++ frontend/app/page.js
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">
+        hello
+      </div>
+      <Button variant='primary' size='xl'> Button </Button>
+    </>
+  )
+}
+
+export default Home
